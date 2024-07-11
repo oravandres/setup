094d113a871a9135b4a6bacbaa01c84aac0e1b8e
Andres Orav <andres.orav@gmail.com>
2024-07-11 19:57:43 +0300

    Add Postgres

--- setup.sh
+++ setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Set the default environment if not provided
-env=${1:-ubuntu}
+# Ensure a playbook is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <playbook>"
+  exit 1
+fi
+
+# Set the playbook
+playbook=$1
 
 # Update package list and upgrade all packages
 sudo apt update && sudo apt upgrade -y
-
-# Install common dependencies
-sudo apt install -y software-properties-common curl git
 
 # Install Ansible
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
 # Run Ansible playbook
-ansible-playbook playbooks/setup.yml -e env=${env}
+ansible-playbook ${playbook}
